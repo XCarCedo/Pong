d7870b1da4156c119dd8df18c6dc71810a0e177e
CarCedo <CarCedo.Pro@gmail.com>
2023-08-09 15:40:52 +0330

    Added movement scripts for paddles

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -6,11 +6,26 @@ enum PaddleSide{
 }
 
 @export var paddle_side: PaddleSide = PaddleSide.player1
+@export var speed:int = 400
 var paddle_textures: Dictionary = {
 	PaddleSide.player1:preload("res://Assets/Game/paddleBlue.png"),
 	PaddleSide.player2:preload("res://Assets/Game/paddleRed.png"),
 }
 
-
 func _ready():
 	$Sprite2D.set_texture(paddle_textures[paddle_side])
+
+func _physics_process(delta):
+	if paddle_side == PaddleSide.player1:
+		if Input.get_action_strength("p1_up"):
+			velocity.y -= speed
+		elif Input.get_action_strength("p1_down"):
+			velocity.y += speed
+	elif paddle_side == PaddleSide.player2:
+		if Input.get_action_strength("p2_up"):
+			velocity.y -= speed
+		elif Input.get_action_strength("p2_down"):
+			velocity.y += speed
+	
+	move_and_slide()
+	velocity.y = 0
